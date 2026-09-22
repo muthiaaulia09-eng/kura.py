d5b5427133271c285701748267a8a21e9d55934b
narzzz16 <nabilaazzahra1416@gmail.com>
2026-09-22 16:20:19 +0800

    hai

---
+++ kura.py
@@ -0,0 +1,20 @@
+import turtle
+
+# Membuka layar untuk menggambar
+layar = turtle.Screen()
+layar.title("Contoh Gambar Turtle")
+layar.bgcolor("black")
+
+# Membuat objek turtle
+pen = turtle.Turtle()
+pen.shape("turtle")
+pen.color("cyan")
+pen.speed(3)
+
+# Menggambar persegi
+for _ in range(4):
+    pen.forward(100)  # Maju 100 piksel
+    pen.left(90)      # Belok kiri 90 derajat
+
+# Menjaga agar jendela tidak langsung tertutup
+turtle.done()
